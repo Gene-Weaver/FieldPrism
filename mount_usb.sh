be824e942009b38ef01a1c1aa4875a3cf89301bc
Gene-Weaver <willwe@umich.edu>
2022-10-12 19:04:48 -0400

    update

--- mount_usb.sh
+++ mount_usb.sh
@@ -4,8 +4,8 @@ echo "     View USB Devices..."
 lsusb
 echo "     If USB storage is not in above list, then the USB drive is likely formatted incorrectly." 
 echo "     Mounting USB Drives..."
-pmount /dev/sda1
-pmount /dev/sdb1
+pmount /dev/sda1 pi
+pmount /dev/sdb1 pi
 echo "     If below is empty, then no drives were mounted. If non-empty, success."
 ls /media/pi/
 echo "Finished"  
