c250a555e4bdfde5db0167261921eb10dd994c97
Gene-Weaver <willwe@umich.edu>
2022-10-12 14:53:47 -0400

    update

--- field_station.py
+++ field_station.py
@@ -42,7 +42,7 @@ def main():
     pipeline = dai.Pipeline()
 
     camRgb = pipeline.create(dai.node.ColorCamera)
-    camRgb.setPreviewSize(1920, 1080)
+    camRgb.setPreviewSize(640, 480)
     camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
     camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
 
