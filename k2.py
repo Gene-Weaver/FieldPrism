d2aed1a1451a90fc4d19e2653808e2c95407d21a
Gene-Weaver <willwe@umich.edu>
2022-10-11 22:34:19 -0400

    update

---
+++ k2.py
@@ -0,0 +1,13 @@
+from sshkeyboard import listen_keyboard
+
+def press(key):
+    print(f"'{key}' pressed")
+
+def release(key):
+    print(f"'{key}' released")
+
+while True:
+    listen_keyboard(
+        on_press=press,
+        on_release=release,
+    )
