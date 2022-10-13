199103df6365133d15a26613b51415090e86ffce
Gene-Weaver <willwe@umich.edu>
2022-10-13 17:43:05 -0400

    update

--- field_station_still.py
+++ field_station_still.py
@@ -7,8 +7,7 @@ import depthai as dai
 import keyboard
 from dataclasses import dataclass
 from utils import bcolors
-import subprocess
-
+import matplotlib.pyplot as plt
 # print(f"{bcolors.OKGREEN}     {bcolors.ENDC}")
 
 @dataclass
@@ -112,7 +111,8 @@ def align_camera():
         while True:
             
             inRgb = qRgb.get()
-            cv2.imshow("rgb", cv2.rotate(inRgb.getCvFrame(), cv2.ROTATE_180))
+            plt.imshow(cv2.rotate(inRgb.getCvFrame(), cv2.ROTATE_180))
+            # cv2.imshow("rgb", cv2.rotate(inRgb.getCvFrame(), cv2.ROTATE_180))
             # time.sleep(1)
 
             # if inRgb is not None:
@@ -232,7 +232,8 @@ def main():
                 save_frame = cv2.rotate(save_frame, cv2.ROTATE_180)
                 frame = cv2.pyrDown(save_frame)
                 frame = cv2.pyrDown(frame)  
-                cv2.imshow('still', frame)
+                plt.imshow(frame)
+                # cv2.imshow('still', frame)
                 # Save
                 route_save_image(cfg,save_frame)
                 TAKE_PHOTO = False
