0a1a3fff24e9b6a02b537dcb202e3a57c924ab41
Gene-Weaver <willwe@umich.edu>
2022-10-14 21:59:32 -0400

    update

--- run_gps.py
+++ run_gps.py
@@ -29,17 +29,48 @@ To see raw data from GPS module, in terminal:
 from dataclasses import dataclass
 import time
 from gps3 import gps3
+from utils import bcolors
 
+'''
+See the section: 
+    CORE PROTOCOL RESPONSES ==> TPV
+    https://gpsd.gitlab.io/gpsd/gpsd_json.html
+for more info about each parameter
+'''
 @dataclass
 class GPSPacket:
-    altitude: float = -999
-    Latitude: float = -999
-    Longitude: float = -999
-    altitude: float = -999
+    time: float = -999
+    device: float = -999
+    mode: float = -999
+    mode_plain: float = -999
 
+    latitude: float = -999
+    longitude: float = -999    
+    
+    altitude: float = -999
+    climb: float = -999
+    speed: float = -999
+
+    lat_error_est: float = -999
+    lon_error_est: float = -999
+    alt_error_est: float = -999
+    
+    def print_report():
+        print(f"{bcolors.OKGREEN}GPS Report{bcolors.ENDC}")
+        print(f"")
+        print(f"{bcolors.OKGREEN}     Lat: {bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Lat: {bcolors.ENDC}")
+        print(f"")
+        print(f"{bcolors.OKGREEN}     Altitude (m): {bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Climb: {bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Speed: {bcolors.ENDC}")
+        print(f"")
+        print(f"{bcolors.OKGREEN}     Climb: {bcolors.ENDC}")
 
 
 def get_gps():
+    GPS_data = GPSPacket()
+
     gps_socket = gps3.GPSDSocket()
     data_stream = gps3.DataStream()
     gps_socket.connect()
@@ -47,7 +78,7 @@ def get_gps():
     for new_data in gps_socket:
         if new_data:
             data_stream.unpack(new_data)
-            print(data_stream.TPV)
+            print(data_stream)
             print('Altitude = ', data_stream.TPV['alt'])
             print('Latitude = ', data_stream.TPV['lat'])
             print('Longitude = ', data_stream.TPV['lon'])
@@ -59,7 +90,6 @@ def get_gps():
             print('Latitude error estimate = ', data_stream.TPV['epy'])
             print('Estimated vertical error = ', data_stream.TPV['epv'])
             print('speed = ', data_stream.TPV['speed'])
-            time.sleep(2)
 
 if __name__ == '__main__':
     get_gps()
