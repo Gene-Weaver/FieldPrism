ab910b10350dc2a5518aa8cd8acc0379d42fc6cc
Gene-Weaver <willwe@umich.edu>
2022-10-14 22:28:24 -0400

    update

--- run_gps.py
+++ run_gps.py
@@ -29,6 +29,7 @@ To see raw data from GPS module, in terminal:
 from dataclasses import dataclass
 import time
 from gps3 import gps3
+from gps3.agps3threaded import AGPS3mechanism
 from utils import bcolors
 
 '''
@@ -93,30 +94,58 @@ def get_gps():
     gps_socket.connect()
     gps_socket.watch()
 
+    agps_thread = AGPS3mechanism()  # Instantiate AGPS3 Mechanisms
+    agps_thread.stream_data()  # From localhost (), or other hosts, by example, (host='gps.ddns.net')
+    agps_thread.run_thread()  # Throttle time to sleep after an empty lookup, default '()' 0.2 two tenths of a second
+
+    count = 0
+    count_fail = 0
+    while True:  # All data is available via instantiated thread data stream attribute.
+        # line #140-ff of /usr/local/lib/python3.5/dist-packages/gps3/agps.py
+        print('---------------------')
+        print(                   agps_thread.data_stream.time)
+        print('Lat:{}   '.format(agps_thread.data_stream.lat))
+        print('Lon:{}   '.format(agps_thread.data_stream.lon))
+        print('Speed:{} '.format(agps_thread.data_stream.alt))
+        print('Speed:{} '.format(agps_thread.data_stream.speed))
+        print('Course:{}'.format(agps_thread.data_stream.track))
+        print('---------------------')
+        # sleep(60) # Sleep, or do other things for as long as you like.
+        if agps_thread.data_stream.lat is not None:
+            print('YES')
+            count += 1
+        else:
+            print('No')
+            count_fail += 1
+        
+        if count_fail > 20 or count_fail >= 10:
+            print('ENDING')
+            break
+
     
-    for new_data in gps_socket:
-        if new_data:
-            for count in range(0,10):
-                print(f'Try {count}')
-                data_stream.unpack(new_data)
+    # for new_data in gps_socket:
+    #     if new_data:
+    #         for count in range(0,10):
+    #             print(f'Try {count}')
+    #             data_stream.unpack(new_data)
 
-                update_GPS_data(data_stream, 'lat')
-                update_GPS_data(data_stream, 'lon')
-                update_GPS_data(data_stream, 'alt')
-                update_GPS_data(data_stream, 'time')
+    #             update_GPS_data(data_stream, 'lat')
+    #             update_GPS_data(data_stream, 'lon')
+    #             update_GPS_data(data_stream, 'alt')
+    #             update_GPS_data(data_stream, 'time')
 
 
 
-                # print('device = ', data_stream.TPV['device'])
-                # print('mode = ', data_stream.TPV['mode'])
-                # print('time = ', data_stream.TPV['time'])
-                # print('climb = ', data_stream.TPV['climb'])
-                # print('Longitude error estimate = ', data_stream.TPV['epx'])
-                # print('Latitude error estimate = ', data_stream.TPV['epy'])
-                # print('Estimated vertical error = ', data_stream.TPV['epv'])
-                # print('speed = ', data_stream.TPV['speed'])
-                if count == 9:
-                    break
-    print('End')
+    #             # print('device = ', data_stream.TPV['device'])
+    #             # print('mode = ', data_stream.TPV['mode'])
+    #             # print('time = ', data_stream.TPV['time'])
+    #             # print('climb = ', data_stream.TPV['climb'])
+    #             # print('Longitude error estimate = ', data_stream.TPV['epx'])
+    #             # print('Latitude error estimate = ', data_stream.TPV['epy'])
+    #             # print('Estimated vertical error = ', data_stream.TPV['epv'])
+    #             # print('speed = ', data_stream.TPV['speed'])
+    #             if count == 9:
+    #                 break
+    # print('End')
 if __name__ == '__main__':
     get_gps()
