5b39e48d45484a908ac8383ca3747380519c4ead
Gene-Weaver <willwe@umich.edu>
2022-10-14 22:18:11 -0400

    update

--- run_gps.py
+++ run_gps.py
@@ -39,7 +39,7 @@ for more info about each parameter
 '''
 @dataclass
 class GPSPacket:
-    time: float = -999
+    current_time: float = -999
     device: float = -999
     mode: float = -999
     mode_plain: float = -999
@@ -54,19 +54,36 @@ class GPSPacket:
     lat_error_est: float = -999
     lon_error_est: float = -999
     alt_error_est: float = -999
+
+    # def update_values(self) -> float:
+    #     if 
+    #     return self.unit_price * self.quantity_on_hand
+
+    # def update_values_str(self) -> str:
+    #     return self.unit_price * self.quantity_on_hand
     
-    def print_report():
+    def total_cost(self) -> None:
         print(f"{bcolors.OKGREEN}GPS Report{bcolors.ENDC}")
         print(f"")
-        print(f"{bcolors.OKGREEN}     Lat: {bcolors.ENDC}")
-        print(f"{bcolors.OKGREEN}     Lat: {bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Time: {str(self.current_time)}{bcolors.ENDC}")
         print(f"")
-        print(f"{bcolors.OKGREEN}     Altitude (m): {bcolors.ENDC}")
-        print(f"{bcolors.OKGREEN}     Climb: {bcolors.ENDC}")
-        print(f"{bcolors.OKGREEN}     Speed: {bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Latitude: {str(self.)}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Longitude: {str(self.)}{bcolors.ENDC}")
         print(f"")
-        print(f"{bcolors.OKGREEN}     Climb: {bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Altitude (m): {str(self.)}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Climb: {str(self.)}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Speed: {str(self.)}{bcolors.ENDC}")
+        print(f"")
+        print(f"{bcolors.OKGREEN}     Lat error estimate: {self.}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Lon error estimate: {self.}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Alt error estimate: {self.}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Climb: {}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Climb: {}{bcolors.ENDC}")
+        print(f"{bcolors.OKGREEN}     Climb: {}{bcolors.ENDC}")
 
+def update_GPS_data(data_stream, item):
+    if data_stream.TPV[item] is not None:
+        print(data_stream.TPV[item])
 
 def get_gps():
     GPS_data = GPSPacket()
@@ -75,21 +92,27 @@ def get_gps():
     data_stream = gps3.DataStream()
     gps_socket.connect()
     gps_socket.watch()
-    for new_data in gps_socket:
-        if new_data:
-            data_stream.unpack(new_data)
-            print(data_stream)
-            print('Altitude = ', data_stream.TPV['alt'])
-            print('Latitude = ', data_stream.TPV['lat'])
-            print('Longitude = ', data_stream.TPV['lon'])
-            print('device = ', data_stream.TPV['device'])
-            print('mode = ', data_stream.TPV['mode'])
-            print('time = ', data_stream.TPV['time'])
-            print('climb = ', data_stream.TPV['climb'])
-            print('Longitude error estimate = ', data_stream.TPV['epx'])
-            print('Latitude error estimate = ', data_stream.TPV['epy'])
-            print('Estimated vertical error = ', data_stream.TPV['epv'])
-            print('speed = ', data_stream.TPV['speed'])
 
+    for count in range(0,4):
+        for new_data in gps_socket:
+            if new_data:
+                print(f'Try {count}')
+                data_stream.unpack(new_data)
+
+                update_GPS_data(data_stream, 'lat')
+                update_GPS_data(data_stream, 'lon')
+                update_GPS_data(data_stream, 'alt')
+                update_GPS_data(data_stream, 'time')
+
+
+
+                # print('device = ', data_stream.TPV['device'])
+                # print('mode = ', data_stream.TPV['mode'])
+                # print('time = ', data_stream.TPV['time'])
+                # print('climb = ', data_stream.TPV['climb'])
+                # print('Longitude error estimate = ', data_stream.TPV['epx'])
+                # print('Latitude error estimate = ', data_stream.TPV['epy'])
+                # print('Estimated vertical error = ', data_stream.TPV['epv'])
+                # print('speed = ', data_stream.TPV['speed'])
 if __name__ == '__main__':
     get_gps()
