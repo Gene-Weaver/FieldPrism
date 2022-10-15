eb66a583bf7a77465a24b4fb1ecfea749d0fae0a
Gene-Weaver <willwe@umich.edu>
2022-10-15 00:02:31 -0400

    update

--- run_gps.py
+++ run_gps.py
@@ -119,7 +119,7 @@ def get_gps(speed):
         else:
             # print('No')
             count_fail += 1
-            time.sleep(0.1)
+            time.sleep(0.05)
         
         if count > max_count:
             # print('SUCCESS')
