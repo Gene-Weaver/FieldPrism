9cabb91a897f428b04e97fd6af4573ec886d4f06
Gene-Weaver <willwe@umich.edu>
2022-10-17 03:45:15 -0400

    update

--- field_station_still.py
+++ field_station_still.py
@@ -395,9 +395,9 @@ def main():
             if keyboard.is_pressed('6'):
                 break
             elif keyboard.is_pressed('1'):
-                ctrl = dai.CameraControl()
-                ctrl.setCaptureStill(True)
-                configQueue.send(ctrl)
+                # ctrl = dai.CameraControl()
+                # ctrl.setCaptureStill(True)
+                # configQueue.send(ctrl)
                 # TAKE_PHOTO = True
                 print(f"       Camera Activated")
                 print(f"       Capturing Image")
