8e94f42b0e558756fd27103785b13f8673db36ae
Gene-Weaver <willwe@umich.edu>
2022-10-17 01:15:24 -0400

    update

--- mount_usb_drives.py
+++ mount_usb_drives.py
@@ -6,7 +6,7 @@ import subprocess
 
 def mount_usb():
     result = subprocess.run(["sh", "./mount_usb_drives.sh"], stderr=subprocess.PIPE, text=True)
-    # print(result.stderr) # normal to see an error if this is printed, but usually there is no concern
+    print(result.stderr) # normal to see an error if this is printed, but usually there is no concern
     # will see the following if drives have never been mounted due to calling pumount(), that's what we want
     #        Error: device /dev/sda1 is not mounted
     #        Error: device /dev/sdb1 is not mounted
