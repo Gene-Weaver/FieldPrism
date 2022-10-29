7ff067584e1645d508b01a0ad5d13dada20e5f66
Gene-Weaver <willwe@umich.edu>
2022-10-29 19:38:09 -0400

    updates

--- mount_usb_drives.sh
+++ mount_usb_drives.sh
@@ -39,35 +39,35 @@ fi
 ######### USB 2 ############
 if [ -e /dev/sdb1 ] ; then
 echo "USB2 mounted"
-pmount -w /dev/sda1 USB2
+pmount -w /dev/sdb1 USB2
 else
 echo "USB2 not connected"
 fi
 ######### USB 3 ############
 if [ -e /dev/sdc1 ] ; then
 echo "USB3 mounted"
-pmount -w /dev/sda1 USB3
+pmount -w /dev/sdc1 USB3
 else
 echo "USB3 not connected"
 fi
 ######### USB 4 ############
 if [ -e /dev/sdd1 ] ; then
 echo "USB4 mounted"
-pmount -w /dev/sda1 USB4
+pmount -w /dev/sdd1 USB4
 else
 echo "USB4 not connected"
 fi
 ######### USB 5 ############
 if [ -e /dev/sde1 ] ; then
 echo "USB5 mounted"
-pmount -w /dev/sda1 USB5
+pmount -w /dev/sde1 USB5
 else
 echo "USB5 not connected"
 fi
 ######### USB 6 ############
 if [ -e /dev/sdf1 ] ; then
 echo "USB6 mounted"
-pmount -w /dev/sda1 USB6
+pmount -w /dev/sdf1 USB6
 else
 echo "USB6 not connected"
 fi
