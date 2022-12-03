507569da7bf022b5902d0c37fcce97ba54f2ff33
Gene-Weaver <willwe@umich.edu>
2022-12-03 17:41:39 -0500

    adding fieldprism

---
+++ fieldstation/create_rpi_image.sh
@@ -0,0 +1,22 @@
+echo "Type target USB device name. This will be in /media/..."
+echo "eg."
+echo "     USB1"
+read USB
+echo ""
+echo "Type R Pi image file name"
+echo "eg."
+echo "     FieldPrism-A_v-1-0"
+read FILENAME
+echo ""
+echo "Type the max size of the image. Default image = 17000"
+echo "eg."
+echo "     FieldPrism-A_v-1-0"
+read SIZE
+
+dir_target = "/media/${USB}/${FILENAME}.img"
+
+sudo dd if=/dev/mmcblk0 of="${dir_target}" bs=1M count="${SIZE}"
+
+cd /media/"${USB}"
+
+sudo pishrink.sh -z ${FILENAME}.img
