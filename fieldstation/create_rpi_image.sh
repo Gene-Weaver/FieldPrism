e9d97533046770d99243c48fb0ece5b442f1bc4c
Gene-Weaver <willwe@umich.edu>
2022-12-03 17:57:18 -0500

    adding fieldprism

--- fieldstation/create_rpi_image.sh
+++ fieldstation/create_rpi_image.sh
@@ -1,26 +1,22 @@
-echo "Type target USB device name. This will be in /media/..."
-echo "eg."
-echo "     USB1"
-read USB
-echo ""
-echo "Type R Pi image file name"
-echo "eg."
-echo "     FieldPrism-A_v-1-0"
-read FILENAME
-echo ""
-echo "Type the max size of the image. Default image = 17000"
-echo "eg."
-echo "     FieldPrism-A_v-1-0"
+#!/bin/sh
+
+# Prompt the user for the directory
+echo "Enter the directory for the USB drive (e.g. /media/USB1):"
+read USB_DIR
+
+# Prompt the user for the image name
+echo "Enter the name for the image (e.g. FieldPrism-A_v-1-0.img):"
+read IMAGE_NAME
+
+# Prompt the user for the size
+echo "Enter the size of the image in MB (e.g. 17000):"
 read SIZE
-$dir_target="/media/"
-$dir_target+="${USB}"
-$dir_target+="/"
-$dir_target+="${FILENAME}"
-$dir_target+=".img"
-echo "dir_target = "
-echo "$dir_target"
-sudo dd if=/dev/mmcblk0 of="${dir_target}" bs=1M count="${SIZE}"
 
-cd /media/"${USB}"
+# Run the dd command with the user-specified values
+sudo dd if=/dev/mmcblk0 of="$USB_DIR/$IMAGE_NAME" bs=1M count="$SIZE"
 
-sudo pishrink.sh -z ${FILENAME}.img
+# Change to the USB drive directory
+cd "$USB_DIR"
+
+# Run the pishrink.sh script with the user-specified image name
+sudo pishrink.sh -z "$IMAGE_NAME"
