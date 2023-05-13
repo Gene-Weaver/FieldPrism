2bef056f3a26fcbd5a74b4e0e404ea88e2d0391b
Gene-Weaver <willwe@umich.edu>
2023-05-13 01:47:59 -0400

    qr code checker

--- fieldstation/utils_gui.py
+++ fieldstation/utils_gui.py
@@ -354,16 +354,18 @@ def config_gui(root, software_version):
 
     # Callback function for radio buttons
     radio_var = tk.StringVar(value="standard")
-    # use_enhanced = False
+    use_enhanced = False
+
     def radio_selected():
+        global use_enhanced
         selection = radio_var.get()
         if selection == "standard":
-            pass
+            use_enhanced = False
         elif selection == "enhance":
-            pass
-    # -------------- Level 6
+            use_enhanced = True
+
     rv_start += 2
-    frame_mode_L = tk.Frame(master=frame_qr_data, height=60, width = 100, bg="black")
+    frame_mode_L = tk.Frame(master=frame_qr_data, height=60, width=100, bg="black")
     frame_mode_L.grid(row=rv_start, column=0, sticky="nsew")
     frame_mode_L.rowconfigure(0, minsize=30)
     frame_mode_L.columnconfigure([0], minsize=50)
@@ -378,11 +380,12 @@ def config_gui(root, software_version):
 
     # Create the radio buttons
     standard_radio = tk.Radiobutton(frame_mode, text="Standard", variable=radio_var, value="standard", command=radio_selected,
-                               bg="black", fg="white", font=("Calibri ", 16))
+                                    bg="black", fg="white", font=("Calibri ", 16), highlightthickness=0)
     standard_radio.grid(row=0, column=0, sticky="w")
     enhance_radio = tk.Radiobutton(frame_mode, text="Enhanced", variable=radio_var, value="enhance", command=radio_selected,
-                               bg="black", fg="white", font=("Calibri ", 16))
+                                bg="black", fg="white", font=("Calibri ", 16), highlightthickness=0)
     enhance_radio.grid(row=1, column=0, sticky="w")
+
 
     return root, frame_preview, frame_saved, label_camera_status, label_focus_live_status, label_focus_saved_status, label_fname_status, label_gps_status, label_gps_lat_status, label_gps_lon_status, label_gps_time_status, label_local_time_status, label_total_status, label_session_status, label_csv_status, label_nimage_status, label_ndevice_status, label_usbspeed_status, label_version_status, label_nqr_status, L1, L2, L3, L4, L5, L6, radio_var
 
