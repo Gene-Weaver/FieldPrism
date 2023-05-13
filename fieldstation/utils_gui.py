9e846a38ec539e42a4406f87109f96f6ba01f287
Gene-Weaver <willwe@umich.edu>
2023-05-12 20:53:46 -0400

    qr code checker

--- fieldstation/utils_gui.py
+++ fieldstation/utils_gui.py
@@ -200,11 +200,11 @@ def config_gui(root, software_version):
 
     n_qr = tk.IntVar(value=0)
 
-    label_nqr = tk.Label(master=frame_info, text="# of QR Codes: ", bg="black", fg="White", font=("Calibri ", 16))
+    label_nqr = tk.Label(master=frame_info_nqr, text="# of QR Codes: ", bg="black", fg="White", font=("Calibri ", 16))
     label_nqr.grid(row=0, column=0, sticky="e")
 
     frame_buttons = tk.Frame(master=frame_info_nqr, bg="black")
-    frame_buttons.grid(row=0, column=0, sticky="nsew")
+    frame_buttons.grid(row=0, column=1, sticky="nsew")
     frame_buttons.columnconfigure([0, 1, 2], weight=1)
 
     def increase_nqr():
