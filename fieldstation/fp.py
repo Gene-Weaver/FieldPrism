a8478740a30095562b187024ca8c3691c3c4a00a
Gene-Weaver <willwe@umich.edu>
2023-05-15 17:20:15 -0400

    qr code checker

--- fieldstation/fp.py
+++ fieldstation/fp.py
@@ -366,7 +366,7 @@ def run_gps_acc_test(label_camera_status, cfg, cfg_user, gps_acc, agps_thread, l
         # Loop 60 times
         n_times = 5
         for nnn in range(n_times):
-            percent = round(np.divide(nnn+1,n_times))
+            percent = round(np.multiply(np.divide(nnn+1,n_times),100))
             label_camera_status.config(text = f'{n_times} Min GPS Test - {percent}%', fg='goldenrod')
             GPS_data = gps_activate(agps_thread, label_gps_status, label_gps_lat_status, label_gps_lon_status, label_local_time_status, label_gps_time_status, cfg_user,True,True)
             if GPS_data.latitude == -999:
@@ -397,7 +397,7 @@ def run_gps_acc_test(label_camera_status, cfg, cfg_user, gps_acc, agps_thread, l
         # Loop 180 times
         n_times = 10
         for _ in range(10):
-            percent = round(np.divide(nnn+1,n_times))
+            percent = round(np.multiply(np.divide(nnn+1,n_times),100))
             label_camera_status.config(text = f'{n_times} Min GPS Test - {percent}%', fg='goldenrod')
             GPS_data = gps_activate(agps_thread, label_gps_status, label_gps_lat_status, label_gps_lon_status, label_local_time_status, label_gps_time_status, cfg_user,True,True)
             if GPS_data.latitude == -999:
