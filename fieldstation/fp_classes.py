00337f2abdf0f348cf28436e35e8ef80e71cd4a5
Gene-Weaver <willwe@umich.edu>
2023-05-17 18:19:11 -0400

    qr code checker

--- fieldstation/fp_classes.py
+++ fieldstation/fp_classes.py
@@ -544,7 +544,7 @@ class GPSTest:
             print(f"N = {len(cluster_coords)}")
 
             # Create a map centered at the average of the coordinates
-            self.map_gps = folium.Map(location=[center_lat, center_lon], zoom_start=22, tiles='CartoDB Positron')
+            self.map_gps = folium.Map(location=[center_lat, center_lon], zoom_start=18, tiles='Stamen Terrain')
             ### These are nice to have, but open very slowly on the RPi...
             # self.map_gps = folium.Map(location=[center_lat, center_lon], zoom_start=22, tiles='Stamen Terrain')
             # folium.TileLayer('cartodbdark_matter').add_to(self.map_gps)
