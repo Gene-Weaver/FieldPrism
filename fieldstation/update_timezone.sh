19524ee98eae7f75c3f1bd8abf41f4a352572e8a
Gene-Weaver <willwe@umich.edu>
2023-05-24 18:45:45 -0400

    timezone

---
+++ fieldstation/update_timezone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if the script is being run with root privileges
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script needs to be run with root privileges. Please use sudo or run as root."
+    exit 1
+fi
+
+# Check if the raspi-config package is installed
+if ! dpkg -s raspi-config >/dev/null 2>&1; then
+    echo "raspi-config package is not installed. Please install it using 'sudo apt-get install raspi-config' and try again."
+    exit 1
+fi
+
+# Run raspi-config with the non-interactive flag to launch the tool
+raspi-config nonint do_change_timezone
+
+# Check the exit status of raspi-config
+if [[ $? -eq 0 ]]; then
+    echo "Timezone changed successfully."
+else
+    echo "Failed to change the timezone."
+fi
