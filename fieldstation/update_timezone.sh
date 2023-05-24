12c9479c52589e7008eb7190e7db0bc1f71efcd4
Gene-Weaver <willwe@umich.edu>
2023-05-24 18:53:48 -0400

    timezone

--- fieldstation/update_timezone.sh
+++ fieldstation/update_timezone.sh
@@ -12,15 +12,29 @@ if ! dpkg -s raspi-config >/dev/null 2>&1; then
     exit 1
 fi
 
-# Run raspi-config with the non-interactive flag to launch the tool
-raspi-config nonint do_change_timezone
+# Prompt the user to select the Wi-Fi country
+echo "Available Wi-Fi country options:"
+raspi-config nonint get_wifi_country | awk -F "[()]" '{ print $2 }'
+read -p "Enter the Wi-Fi country code (e.g., HR for Croatia): " wifi_country
+
+# Set the Wi-Fi country using raspi-config
+raspi-config nonint do_wifi_country "$wifi_country"
+
+# Prompt the user to select the timezone
+echo "Available timezone options:"
+raspi-config nonint get_current_timezone
+read -p "Enter the timezone (e.g., Europe/Zagreb): " timezone
+
+# Set the timezone using raspi-config
+raspi-config nonint do_change_timezone "$timezone"
 
 # Check the exit status of raspi-config
 if [ $? -eq 0 ]; then
-    echo "Timezone changed successfully."
+    echo "Wi-Fi country and timezone updated successfully."
 else
-    echo "Failed to change the timezone."
+    echo "Failed to update Wi-Fi country and timezone."
 fi
+
 echo ""
 echo "This window will automatically close in 10 seconds..."
 sleep 10
